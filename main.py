af530ae882539a60a42606c60a19e569251625fd
Xaalf104 <alsharief.dinglasa06@gmail.com>
2024-03-01 15:23:03 +0800

    Type 0 added

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ rf_classifier = load("./models/trained_model.joblib")
 # Load the MultiLabelBinarizer instance
 mlb = load("./models/mlb_model.joblib")
 
-@app.route('/pre-defined', methods=['POST'])
+
+@app.route("/pre-defined", methods=["POST"])
 def process_json():
     try:
 
@@ -22,15 +23,20 @@ def process_json():
 
         # Convert json_data to a JSON-formatted string
 
-
         reports_wc, reports_cy, reports_ny = generate_individual_reports(df)
 
-        return jsonify({"withered_reports": reports_wc, "crop_yield": reports_cy, "net_yield":  reports_ny})
+        return jsonify(
+            {
+                "withered_reports": reports_wc,
+                "crop_yield": reports_cy,
+                "net_yield": reports_ny,
+            }
+        )
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
 
 
-@app.route('/suggested-tags', methods=['POST'])
+@app.route("/suggested-tags", methods=["POST"])
 def get_suggested_tags():
     try:
         json_data = request.get_json()
@@ -38,7 +44,9 @@ def get_suggested_tags():
         # Load new samples without tags
 
         json_data_string = json.dumps(json_data)
-        new_samples = pd.read_json(io.StringIO(json_data_string[1:-1]), orient='records', lines=True)
+        new_samples = pd.read_json(
+            io.StringIO(json_data_string[1:-1]), orient="records", lines=True
+        )
 
         # Preprocess the new samples (similar to training data preprocessing)
         new_samples_features = new_samples[
@@ -57,7 +65,9 @@ def get_suggested_tags():
         predicted_tags_new_samples = mlb.inverse_transform(new_samples_predictions)
 
         # Convert the list of tuples into a list of lists with removed spaces
-        suggested_tags = [[tag.strip() for tag in tags] for tags in predicted_tags_new_samples]
+        suggested_tags = [
+            [tag.strip() for tag in tags] for tags in predicted_tags_new_samples
+        ]
 
         # Define the tags to check for in the suggested tags
         tags_to_check = [
@@ -134,7 +144,6 @@ def compare_growth(average_growth, recent_growth):
         return "No significant increase in growth rate."
 
 
-
 def generate_individual_reports(dataframe):
     # lists to store messages for each plant
     report_wc = []
@@ -155,7 +164,8 @@ def generate_individual_reports(dataframe):
     excellent_ny = "Net yield is commendable"
     bad_ny = "Net yield is below expectations"
     average_ny = "Net yield is average"
-    
+    terrible_ny = "Net yield is negative, indicating significant losses"
+
     for index, row in dataframe.iterrows():
         report = f"Report for {row['plant']} crop:\n"
 
@@ -195,13 +205,55 @@ def generate_individual_reports(dataframe):
             report += f"net yield is below expectations\n"
             report_ny.append((row["plant"] + ": " + bad_ny))
 
+        # Type 0
+        if row["withered_crops"] > 5 and row["type"] == 0:
+            report += f"Withered crops have significantly impacted yield. Immediate action is needed. "
+            report_wc.append((row["plant"] + ": " + terrible_wc))
+        elif row["withered_crops"] >= 3 and row["type"] == 0:
+            report += f"The number of withered crops is concerning and impacting yield "
+            report_wc.append((row["plant"] + ": " + bad_wc))
+        elif (
+            row["withered_crops"] >= 1
+            and row["withered_crops"] < 3
+            and row["type"] == 0
+        ):
+            report += (
+                f"There are some losses due to withered crops, but they're manageable "
+            )
+            report_wc.append(row["plant"] + ": " + mild_wc)
+        else:
+            report_wc.append(row["plant"] + ": " + good_wc)
+
+        #  crop yield
+        if row["crop_yield"] == 1 and row["type"] == 0:
+            report += f"crop yield is satisfactory "
+            report_cy.append(row["plant"] + ": " + average_cy)
+        elif row["crop_yield"] < 1 and row["crop_yield"] > 0 and row["type"] == 0:
+            report += f"Crop yield is below expectations, "
+            report_cy.append(row["plant"] + ": " + bad_cy)
+        elif row["crop_yield"] < 0 and row["type"] == 0:
+            report += f"Crop yield is disastrously low, "
+            report_cy.append(row["plant"] + ": " + terrible_cy)
+        elif row["crop_yield"] > 1 and row["type"] == 0:
+            report += f"Crop yield is exceptional! "
+            report_cy.append(row["plant"] + ": " + excellent_cy)
+
+        # net yield
+        if row["net_yield"] == row["planted_qty"] and row["type"] == 0:
+            report += f"net yield is performing average. \n"
+            report_ny.append(average_ny)
+        elif row["net_yield"] > row["planted_qty"] and row["type"] == 0:
+            report += f"net yield exceeds expectationsyield is commendable. \n"
+            report_ny.append(excellent_ny)
+        elif row["net_yield"] < row["planted_qty"] and row["type"] == 0:
+            report += f"net yield is lower than anticipated\n"
+            report_ny.append(bad_ny)
+        elif row["net_yield"] < 0 and row["type"] == 0:
+            report += f"net yield is negative, indicating significant losses\n"
+            report_ny.append(terrible_ny)
+
     return report_wc, report_cy, report_ny
 
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
-
-
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
